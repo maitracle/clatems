f3d41a6fdfb2d4862840cec90f194a7a3c7b4342
maitracle <39800081+maitracle@users.noreply.github.com>
2021-12-22 16:12:06 +0900

    test: test artwork controller with spring boot

--- src/test/kotlin/com/clatems/clatems/artworks/ArtworkControllerTest.kt
+++ src/test/kotlin/com/clatems/clatems/artworks/ArtworkControllerTest.kt
@@ -1,43 +1,35 @@
 package com.clatems.clatems.artworks
 
-import com.clatems.clatems.commons.DtoConverter
-import com.clatems.clatems.commons.ModelMapperConfig
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import kotlin.streams.toList
 
-internal class ArtworkControllerTest {
-    private val mockArtworkService = Mockito.mock(ArtworkService::class.java)
-    private val modelMapper = ModelMapperConfig().modelMapper()
-    private val dtoConverter = DtoConverter<Artwork>(modelMapper)
-    private val artworkController = ArtworkController(mockArtworkService, dtoConverter)
-
+@SpringBootTest
+internal class ArtworkControllerTest(
+    @Autowired private val artworkController: ArtworkController,
+    @Autowired private val artworkRepository: ArtworkRepository,
+) {
 
     @Test
     fun `should get artwork list`() {
         val expectedArtworkCount = 3
-        val serviceArtworkListResponse = listOf(
-            Artwork(id = 1),
-            Artwork(id = 2),
-            Artwork(id = 3),
+        val expectedArtworkList = artworkRepository.saveAll(
+            listOf(
+                Artwork(id = 1),
+                Artwork(id = 2),
+                Artwork(id = 3),
+            )
         )
-        val expectedArtworkList = serviceArtworkListResponse
-            .stream().map { artwork ->
-                modelMapper.map(artwork, ArtworkResponseDto::class.java)
-            }.toList()
-
-        Mockito.`when`(mockArtworkService.findAll()).thenReturn(serviceArtworkListResponse)
 
         val actual = artworkController.getArtworkList()
 
         Assertions.assertEquals(HttpStatus.OK, actual.statusCode)
 
         val actualArtworkList = actual.body!!.toList()
-
         Assertions.assertEquals(expectedArtworkCount, actualArtworkList.count())
-
         expectedArtworkList.zip(actualArtworkList).forEach { pair ->
             Assertions.assertEquals(pair.first.id, pair.second.id)
             Assertions.assertEquals(pair.first.createdAt, pair.second.createdAt)
@@ -48,16 +40,13 @@ internal class ArtworkControllerTest {
     @Test
     fun `should retrieve artwork`() {
         val artworkIdParam: Long = 1
-        val expectedArtwork = Artwork(id = artworkIdParam)
-
-        Mockito.`when`(mockArtworkService.getById(artworkIdParam)).thenReturn(expectedArtwork)
+        val expectedArtwork = artworkRepository.save(Artwork(id = artworkIdParam))
 
         val actual = artworkController.retrieveArtwork(artworkIdParam)
 
         Assertions.assertEquals(HttpStatus.OK, actual.statusCode)
 
         val actualArtwork = actual.body!!
-
         Assertions.assertEquals(expectedArtwork.id, actualArtwork.id)
         Assertions.assertEquals(expectedArtwork.createdAt, actualArtwork.createdAt)
         Assertions.assertEquals(expectedArtwork.updatedAt, actualArtwork.updatedAt)
@@ -69,10 +58,12 @@ internal class ArtworkControllerTest {
 
     @Test
     fun `should update artwork`() {
+        // Todo(maitracle): artwork에 수정할 만한 field가 추가된 후 test를 보완한다.
         val artworkIdParam: Long = 1
         val updateArtworkDto = UpdateArtworkDto(id = artworkIdParam)
 
         val actual = artworkController.updateArtwork(artworkIdParam, updateArtworkDto)
+
     }
 
     @Test
@@ -82,8 +73,11 @@ internal class ArtworkControllerTest {
     @Test
     fun `should delete artwork`() {
         val artworkIdParam: Long = 1
+        artworkRepository.save(Artwork(id = artworkIdParam))
 
         val actual = artworkController.deleteArtwork(artworkIdParam)
+
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, actual.statusCode)
     }
 
     @Test
